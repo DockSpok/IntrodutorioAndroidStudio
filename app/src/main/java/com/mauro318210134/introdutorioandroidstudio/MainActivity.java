62e5f91b4e166094a8f855abcf98d5c736b92f7b
MAURO SIQUEIRA DAMASCENO <43550691+DockSpok@users.noreply.github.com>
2020-09-13 02:52:10 -0300

    Initial commit

---
+++ app/src/main/java/com/mauro318210134/introdutorioandroidstudio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mauro318210134.introdutorioandroidstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
