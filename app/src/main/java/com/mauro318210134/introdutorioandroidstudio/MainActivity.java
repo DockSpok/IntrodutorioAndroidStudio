0954fdc5249e5222053a4d9e6d57d3db1838b5d1
MAURO SIQUEIRA DAMASCENO <43550691+DockSpok@users.noreply.github.com>
2020-09-14 01:34:07 -0300

    Mapeando as views na MainActivity
    Todos os elementos descendem da classe R que descente de Object
    podem ser localizados por id

--- app/src/main/java/com/mauro318210134/introdutorioandroidstudio/MainActivity.java
+++ app/src/main/java/com/mauro318210134/introdutorioandroidstudio/MainActivity.java
@@ -3,12 +3,20 @@ package com.mauro318210134.introdutorioandroidstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected EditText txtValor;
+    protected Button btnLimpar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtValor.findViewById(R.id.txtValor);
+        btnLimpar.findViewById(R.id.btnLimpar);
     }
 }
